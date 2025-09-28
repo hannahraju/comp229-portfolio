bbb14fb167441caf06bd870c7a88777c8551930a
Hannah Raju <hannahraju96@gmail.com>
2025-09-28 16:47:01 -0400

    added import statement to resume.jsx so file path isn't hardcoded

--- src/resume.jsx
+++ src/resume.jsx
@@ -4,12 +4,14 @@ Student Name: Hannah Raju
 Student ID: 301543568
 Date: 27 September 2025
 */}
+import myresume from "../src/assets/pdfs/my-resume.pdf"
+
 
 export default function Resume() {
     return(
         <div>
         <h2>Resume</h2>
-        <iframe src="../public/my-resume.pdf" width="100%" height="1200px"/>
+        <iframe src={ myresume } width="100%" height="1200px"/>
         </div>
     )
 }
